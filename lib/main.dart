0bd9650d87437f25290c562037233960d1af42a0
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2018-03-08 14:21:02 -0400

    copy ListDemo from listdemo.dart in flutter_gallery

--- lib/main.dart
+++ lib/main.dart
@@ -3,107 +3,257 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeData = new ThemeData(
+      primaryColor: Colors.blueGrey,
+    );
+
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'List Demo from Flutter Gallery',
+      //theme: new ThemeData.dark(),
+      theme: themeData,
+      home: new ListDemo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+enum _MaterialListType {
+  /// A list tile that contains a single line of text.
+  oneLine,
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  /// A list tile that contains a [CircleAvatar] followed by a single line of text.
+  oneLineWithAvatar,
 
-  final String title;
+  /// A list tile that contains two lines of text.
+  twoLine,
 
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  /// A list tile that contains three lines of text.
+  threeLine,
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ListDemo extends StatefulWidget {
+  const ListDemo({ Key key }) : super(key: key);
 
-  void _incrementCounter() {
+  static const String routeName = '/material/list';
+
+  @override
+  _ListDemoState createState() => new _ListDemoState();
+}
+
+class _ListDemoState extends State<ListDemo> {
+  static final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  PersistentBottomSheetController<Null> _bottomSheet;
+  _MaterialListType _itemType = _MaterialListType.threeLine;
+  bool _dense = false;
+  bool _showAvatars = true;
+  bool _showIcons = false;
+  bool _showDividers = false;
+  bool _reverseSort = false;
+  List<String> items = <String>[
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+  ];
+
+  void changeItemType(_MaterialListType type) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _itemType = type;
     });
+    _bottomSheet?.setState(() { });
+  }
+
+  void _showConfigurationSheet() {
+    final PersistentBottomSheetController<Null> bottomSheet = scaffoldKey.currentState.showBottomSheet((BuildContext bottomSheetContext) {
+      return new Container(
+        decoration: const BoxDecoration(
+          border: const Border(top: const BorderSide(color: Colors.black26)),
+        ),
+        child: new ListView(
+          shrinkWrap: true,
+          primary: false,
+          children: <Widget>[
+            new MergeSemantics(
+              child: new ListTile(
+                dense: true,
+                title: const Text('One-line'),
+                trailing: new Radio<_MaterialListType>(
+                  value: _showAvatars ? _MaterialListType.oneLineWithAvatar : _MaterialListType.oneLine,
+                  groupValue: _itemType,
+                  onChanged: changeItemType,
+                )
+              ),
+            ),
+            new MergeSemantics(
+              child: new ListTile(
+                dense: true,
+                title: const Text('Two-line'),
+                trailing: new Radio<_MaterialListType>(
+                  value: _MaterialListType.twoLine,
+                  groupValue: _itemType,
+                  onChanged: changeItemType,
+                )
+              ),
+            ),
+            new MergeSemantics(
+              child: new ListTile(
+                dense: true,
+                title: const Text('Three-line'),
+                trailing: new Radio<_MaterialListType>(
+                  value: _MaterialListType.threeLine,
+                  groupValue: _itemType,
+                  onChanged: changeItemType,
+                ),
+              ),
+            ),
+            new MergeSemantics(
+              child: new ListTile(
+                dense: true,
+                title: const Text('Show avatar'),
+                trailing: new Checkbox(
+                  value: _showAvatars,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _showAvatars = value;
+                    });
+                    _bottomSheet?.setState(() { });
+                  },
+                ),
+              ),
+            ),
+            new MergeSemantics(
+              child: new ListTile(
+                dense: true,
+                title: const Text('Show icon'),
+                trailing: new Checkbox(
+                  value: _showIcons,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _showIcons = value;
+                    });
+                    _bottomSheet?.setState(() { });
+                  },
+                ),
+              ),
+            ),
+            new MergeSemantics(
+              child: new ListTile(
+                dense: true,
+                title: const Text('Show dividers'),
+                trailing: new Checkbox(
+                  value: _showDividers,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _showDividers = value;
+                    });
+                    _bottomSheet?.setState(() { });
+                  },
+                ),
+              ),
+            ),
+            new MergeSemantics(
+              child: new ListTile(
+                dense: true,
+                title: const Text('Dense layout'),
+                trailing: new Checkbox(
+                  value: _dense,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _dense = value;
+                    });
+                    _bottomSheet?.setState(() { });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+
+    setState(() {
+      _bottomSheet = bottomSheet;
+    });
+
+    _bottomSheet.closed.whenComplete(() {
+      if (mounted) {
+        setState(() {
+          _bottomSheet = null;
+        });
+      }
+    });
+  }
+
+  Widget buildListTile(BuildContext context, String item) {
+    Widget secondary;
+    if (_itemType == _MaterialListType.twoLine) {
+      secondary = const Text('Additional item information.');
+    } else if (_itemType == _MaterialListType.threeLine) {
+      secondary = const Text(
+        'Even more additional list item information appears on line three.',
+      );
+    }
+    return new MergeSemantics(
+      child: new ListTile(
+        isThreeLine: _itemType == _MaterialListType.threeLine,
+        dense: _dense,
+        leading: _showAvatars ? new ExcludeSemantics(child: new CircleAvatar(child: new Text(item))) : null,
+        title: new Text('This item represents $item.'),
+        subtitle: secondary,
+        trailing: _showIcons ? new Icon(Icons.info, color: Theme.of(context).disabledColor) : null,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final String layoutText = _dense ? ' \u2013 Dense' : '';
+    String itemTypeText;
+    switch (_itemType) {
+      case _MaterialListType.oneLine:
+      case _MaterialListType.oneLineWithAvatar:
+        itemTypeText = 'Single-line';
+        break;
+      case _MaterialListType.twoLine:
+        itemTypeText = 'Two-line';
+        break;
+      case _MaterialListType.threeLine:
+        itemTypeText = 'Three-line';
+        break;
+    }
+
+    Iterable<Widget> listTiles = items.map((String item) => buildListTile(context, item));
+    if (_showDividers)
+      listTiles = ListTile.divideTiles(context: context, tiles: listTiles);
+
     return new Scaffold(
+      key: scaffoldKey,
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text('Scrolling list\n$itemTypeText$layoutText'),
+        actions: <Widget>[
+          new IconButton(
+            icon: const Icon(Icons.sort_by_alpha),
+            tooltip: 'Sort',
+            onPressed: () {
+              setState(() {
+                _reverseSort = !_reverseSort;
+                items.sort((String a, String b) => _reverseSort ? b.compareTo(a) : a.compareTo(b));
+              });
+            },
+          ),
+          new IconButton(
+            icon: const Icon(Icons.more_vert),
+            tooltip: 'Show menu',
+            onPressed: _bottomSheet == null ? _showConfigurationSheet : null,
+          ),
+        ],
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: new Scrollbar(
+        child: new ListView(
+          padding: new EdgeInsets.symmetric(vertical: _dense ? 4.0 : 8.0),
+          children: listTiles.toList(),
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
